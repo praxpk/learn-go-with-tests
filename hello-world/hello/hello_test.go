710f9b7a38df7df5095f4e587c5420b328701026
Prashanth Kumar Purshotam <23168694+praxpk@users.noreply.github.com>
2026-03-04 19:08:18 -0800

    subtests

--- hello-world/hello/hello_test.go
+++ hello-world/hello/hello_test.go
@@ -10,16 +10,19 @@ func TestHelloGreeting(t *testing.T) {
 		name := "Chris"
 		got := Hello(name)
 		want := fmt.Sprintf("Hello, %s!", name)
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 	t.Run("Returning hello world if no name is give", func(t *testing.T) {
 		want := "Hello, World!"
 		got := Hello("")
-		if got != want {
-			t.Errorf("got %s, want %s", got, want)
-		}
+		assertCorrectMessage(t, got, want)
 	})
 
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
+	if got != want {
+		t.Errorf("got %s, want %s", got, want)
+	}
 }
